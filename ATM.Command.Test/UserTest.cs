412d1024b6b35c54ba6c1dbd59792aa3d8f8a4b7
hankuikuide <hankuikuide@163.com>
2013-08-17 09:31:34 +0800

    Add Testing Code
    Add Testing Code

---
+++ ATM.Command.Test/UserTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ATM.Command.Business;
+namespace CommandPattern
+{
+    [TestClass]
+    public class UserTest
+    {
+
+        [TestMethod]
+        public void checkBalanceTest()
+        {
+            User user = new User("Li", 0);
+
+            Assert.AreEqual(0, user.checkBalance());
+
+            Assert.AreEqual(1000, user.deposit(1000));
+
+            Assert.AreEqual(700, user.withDraw(300));
+
+            Assert.AreEqual(500, user.charge(100));
+
+        }
+
+        //[TestMethod]
+        //public void depositTest()
+        //{
+        //    User user = new User("Li", 0);
+
+            
+        //}
+
+        //[TestMethod]
+        //public void withDrawTest()
+        //{
+        //    User user = new User("Li", 0);
+        //    user.deposit(1000);
+        //    Assert.AreEqual(700, user.withDraw(300));
+        //}
+
+        //[TestMethod]
+        //public void chargeTest()
+        //{
+        //    User user = new User("Li", 0);
+        //    //user.deposit(1000);
+        //    //user.withDraw(300);
+        //    Assert.AreEqual(500, user.charge(100));
+        //}
+
+    }
+}
